0abdc87190c4956607605b24ae3358e41a8f1e8e
Quantum <aburorhambagadaniel@gmail.com>
2026-04-03 13:22:50 +0100

    added hospita Card details

--- src/Components/HospitalCard.jsx
+++ src/Components/HospitalCard.jsx
@@ -2,11 +2,61 @@ import { useState } from 'react';
 import './Booking.css';
 import './HospitalCard.css';
 function Card({ closeModal2, display2, chooseDepartment, department }) {
+    const card = [
+        {
+            cardName: 'Standard Card',
+            Price: '2,500',
+            option1: 'OPD Priority Access',
+            option2: '10% Pharmacy Discount',
+            option3: 'Free Annual Health Screening',
+            option4: 'Patient Portal Access',
+            option5: 'Patient Portal Access'
+
+
+        },
+        {
+            cardName: 'Premium Card',
+            Price: '5,000',
+            option1: 'All Standard Benefits',
+            option2: '20% Pharmacy Discount',
+            option3: 'Free Monthly Lab Tests',
+            option4: 'Specialist Fast-Track',
+            option5: 'Emergency Home Visit (1x/year)',
+        },
+        {
+            cardName: 'Family Card',
+            Price: '12,000',
+            option1: 'Covers up to 6 members',
+            option2: '25% All-services Discount',
+            option3: 'Free Pediatrics check-ups',
+            option4: 'Dedicated Family Doctor',
+            option5: 'Priority Emergency Access',
+        }
+    ];
 
 
 
     const [modalc, setModalc] = useState(
-        <div className="modal-body">
+        <div className="modal-body2 top-4">
+            <div className="card-plan"> <h3>Choose Your Plan</h3> </div>
+            <div>
+                {card.map((cards, i) => {
+                    return (
+                        <div className="plan-items mt-4" key={i}> <div>  <strong> {cards.cardName} </strong> <span className='fs-4'> ₦{cards.Price}</span></div>
+                            <div className="card-details">✔ {cards.option1}</div>
+                            <div className="card-details">✔ {cards.option2}</div>
+                            <div className="card-details">✔ {cards.option3}</div>
+                            <div className="card-details">✔{cards.option4}</div>
+                            <div className="card-details">✔{cards.option5}</div>
+
+
+                        </div>
+                    )
+
+                })}
+
+
+            </div>
 
             <div className="header"></div>
 
